b224e04917650f6bdbedd9610cbdcaf989cda77c
Lindsey <lindsey_mccluskey@yahoo.ca>
2024-12-07 17:01:58 -0800

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,7 +1,26 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt = require('jsonwebtoken')
 
 const restricted = (req, res, next) => {
-  next()
+    const token = req.headers.authorization
+    if(token) {
+      jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if(err){
+          next({
+            status: 401,
+            message: "Token invalid"
+          })
+        } else {
+          req.decodedJwt = decoded
+          next()
+        }
+      })
+    } else {
+      next({
+        status: 401,
+        message: "Token required"
+      })
+    }
   /*
     If the user does not provide a token in the Authorization header:
     status 401
